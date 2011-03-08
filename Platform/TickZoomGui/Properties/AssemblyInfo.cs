526f09cadfb954d21e29edd0f0ff9130edbfb84c
AutoBuild <wayne@tickzoom.com>
2011-03-08 09:49:35 -0500

    Build version 1.1.15.15465.

--- Platform/TickZoomGui/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using TickZoom.Api;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.15.15403")]
+[assembly: AssemblyVersion("1.1.15.15465")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
