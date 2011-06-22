8002f7caca7614f531427f5557cbf623a2c8bab1
AutoBuild <wayne@tickzoom.com>
2011-06-22 09:58:28 -0400

    Build version 1.1.30.30727.

--- Platform/TickZoomGui/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using TickZoom.Api;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.30.30584")]
+[assembly: AssemblyVersion("1.1.30.30727")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- Platform/TickZoomGui2/Properties/AssemblyInfo.cs
+++ Platform/TickZoomGui2/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.30.30584")]
+[assembly: AssemblyVersion("1.1.30.30727")]
 //[assembly: AssemblyFileVersion("2.0.0.0")]
